4a6eb41f8ecdf6d7a45c81e05f16aa53eeb541ca
Drew Conner <drew@blinkingboxes.net>
2023-01-16 07:40:16 -0600

    added vue components

---
+++ frontend/src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import BlogPost from '@/components/BlogPost'
+import BlogAuthor from '@/components/BlogAuthor'
+import PostsByTag from '@/components/PostsByTag'
+import AllPosts from '@/components/AllPosts'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/author/:username', component: BlogAuthor },
+    { path: '/post/:slug', component: BlogPost },
+    { path: '/tag/:tag', component: PostsByTag },
+    { path: '/', component: AllPosts },
+]
+
+const router = new VueRouter({
+    routes: routes,
+    mode: 'history',
+})
+export default router
