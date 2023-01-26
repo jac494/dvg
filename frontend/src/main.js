ac63f246d490be0c8320ce1a182d9a391f4eb640
Drew Conner <drew@blinkingboxes.net>
2023-01-26 16:47:18 -0600

    still trying to make some changes to get frontend working, saving progress but still not there

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 
 // new Vue({
 //     router,
 // })
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router);
+
+app.mount('#app');
+
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import Vue from 'vue'
+import createRouter from 'vue-router'
+
+import BlogPost from '@/components/BlogPost'
+import BlogAuthor from '@/components/BlogAuthor'
+import PostsByTag from '@/components/PostsByTag'
+import AllPosts from '../components/AllPosts'
+
+// Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/author/:username', component: BlogAuthor },
+  { path: '/post/:slug', component: BlogPost },
+  { path: '/tag/:tag', component: PostsByTag },
+  { path: '/', component: AllPosts },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: routes,
+  mode: 'history',
 })
 
 export default router
